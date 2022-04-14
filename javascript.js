3be9601254e9d063b756b621bf40f601504ca0bf
lompdoctor <pleclair@protonmail.com>
2022-04-14 15:51:21 -0500

    Add clear button

--- javascript.js
+++ javascript.js
@@ -11,5 +11,20 @@ function divide(a,b) {
 };
 
 function multiply(a,b) {
-  return a*b;
+    return a*b;
 };
+
+function operate(operator,a,b) {
+    if (operator == 'add') {
+        return add(a,b);
+    } else if (operator == 'subtract') {
+        return subtract(a,b);
+    } else if (operator == 'divide') {
+        return divide(a,b);
+    } else if (operator == 'multiply') {
+        let result = multiply(a,b);
+        return result;
+    } else {
+        console.log('error');
+    }
+}
