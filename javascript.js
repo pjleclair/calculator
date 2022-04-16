7bf2424a53fbb3130dd2e02a735da381d1c42720
lompdoctor <pleclair@protonmail.com>
2022-04-16 13:46:58 -0500

    Map javascript functions to all buttons

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@ function subtract(a,b) {
 };
 
 function divide(a,b) {
-    return (a/b);
+    return (a / b);
 };
 
 function multiply(a,b) {
@@ -15,16 +15,60 @@ function multiply(a,b) {
 };
 
 function operate(operator,a,b) {
-    if (operator == 'add') {
+    if (operator == '+') {
         return add(a,b);
-    } else if (operator == 'subtract') {
+    } else if (operator == '-') {
         return subtract(a,b);
-    } else if (operator == 'divide') {
+    } else if (operator == '%') {
         return divide(a,b);
-    } else if (operator == 'multiply') {
-        let result = multiply(a,b);
-        return result;
+    } else if (operator == 'x') {
+        return multiply(a,b);
     } else {
         console.log('error');
     }
 }
+
+function equals() {
+    num2 = displayBox[0].innerHTML;
+    displayBox[0].innerHTML = operate(operator,+num1,+num2);
+}
+
+function displayNum(num) {
+    let displayText = displayBox[0].innerHTML;
+    displayBox[0].innerHTML = `${displayText+num}`;
+}
+
+let num1, num2, operator, numCount, result;
+numCount = 0;
+const numButtons = document.getElementsByClassName('calc-num');
+const clearButton = document.getElementsByClassName('clear-button');
+const functionButtons = document.getElementsByClassName('function');
+const equalsButton = document.getElementsByClassName('equals-button');
+const displayBox = document.getElementsByClassName('display');
+for (button of numButtons) {
+    let buttonNum = button.innerHTML;
+    button.addEventListener("click", function () {displayNum(buttonNum)});
+}
+
+for (func of functionButtons) {
+    let funcOperator = func.innerHTML;
+    func.addEventListener("click", function() {
+        if (numCount == 0) {
+            num1 = displayBox[0].innerHTML;
+        } else {
+            return;
+        }
+        operator = funcOperator;
+        displayBox[0].innerHTML = '';
+        numCount++;
+    });
+}
+equalsButton[0].addEventListener("click", function(){equals()});
+clearButton[0].addEventListener("click", function(){
+    displayBox[0].innerHTML = '';
+    operator = '';
+    num1 = 0;
+    num2 = 0;
+    numCount = 0;
+});
+
